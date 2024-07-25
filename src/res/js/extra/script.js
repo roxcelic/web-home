af9f61d95e2385061b59f3173d8e5dccd43164d7
roxcelic <roxtheangel@outlook.com>
2024-07-25 16:54:16 +0100

    [prerel 0 -> 0.0] lmost qud
    pre-release, version 0.0, --name--, quick update

--- src/res/js/extra/script.js
+++ src/res/js/extra/script.js
@@ -1,13 +1,182 @@
-export async function fetchDataFromWorker() {
-    try {
-      const response = await fetch("https://api.roxcelic.love/");
+async function fetchData(url, params = {}) {
+  try {
+      const response = await fetch(url, params);
       if (response.ok) {
-        const data = await response.json();
-        return data
+          return await response.json();
       } else {
-        console.error("Error fetching data from Cloudflare Worker:", response.status);
+          console.error("Error fetching data:", response.status);
       }
-    } catch (error) {
+  } catch (error) {
       console.error("An error occurred while fetching data:", error);
-    }
   }
+  return null;
+}
+
+async function fetchDataFromWorker() {
+  return await fetchData("https://api.roxcelic.love/");
+}
+
+async function getAlbum(input, artist) {
+  const url = `https://api.roxcelic.love/?input=${input}&artist=${artist}`;
+  return await fetchData(url);
+}
+
+function createElement(tag, attributes = {}, textContent = "") {
+  const element = document.createElement(tag);
+  for (const key in attributes) {
+      if (attributes.hasOwnProperty(key)) {
+          element.setAttribute(key, attributes[key]);
+      }
+  }
+  element.textContent = textContent;
+  return element;
+}
+
+export async function getDataSpotify() { 
+  const data = await fetchDataFromWorker();
+  if (!data) return;
+
+  const spotifyItem1 = document.getElementById("spotifyicon");
+  const spotifyItem2 = document.getElementById("spotifydata");
+  const spotifyItem3 = document.getElementById("main_playlist");
+
+  spotifyItem1.style.backgroundImage = `url('${data.spotify.pfp[1].url}')`;
+
+  spotifyItem2.appendChild(createElement("p", {}, data.spotify.userName));
+  spotifyItem2.appendChild(createElement("a", { href: data.spotify.link.spotify }, "Come visit my Spotify here!"));
+
+  spotifyItem3.src = `https://open.spotify.com/embed/playlist/${data.spotifyplaylist}?utm_source=generator&theme=1`;
+}
+
+export async function getDataCurrent(host, host1) { 
+  const data = await fetchDataFromWorker();
+  if (!data) return;
+
+  if (data.lastfm['@attr']) {
+
+      const currentItem1 = document.getElementById("currenticon");
+      const currentItem2 = document.getElementById("currentdata");
+      const currentItem3 = document.getElementById("albumname");
+      const currentItem5 = document.getElementById("albumembed");
+
+      currentItem1.style.backgroundImage = `url('${data.lastfm.image[3]['#text']}')`;
+
+      const artistText = `${data.lastfm.name} - by - ${data.lastfm.artist['#text']}`;
+      currentItem2.appendChild(createElement("p", {}, artistText));
+
+      currentItem3.textContent = data.lastfm.album['#text'];
+
+      const albumData = await getAlbum(data.lastfm.album['#text'], data.lastfm.artist['#text']);
+      currentItem5.src = `https://open.spotify.com/embed/album/${albumData.song}?utm_source=generator&theme=1`;
+  } else {
+      host.remove();
+      host1.remove();
+  }
+}
+
+export async function fetchAndDisplayPosts() {
+  const apiUrl = 'https://fedi.roxcelic.love/api/v1/accounts/AjITkomZCGnFUiYbc8/statuses';
+  const accountUrl = 'https://fedi.roxcelic.love/api/v1/accounts/AjITkomZCGnFUiYbc8';
+  try {
+      const accountResponse = await fetch(accountUrl);
+      if (!accountResponse.ok) {
+          throw new Error(`HTTP error! status: ${accountResponse.status}`);
+      }
+      const accountData = await accountResponse.json();
+      const profilePictureUrl = accountData.avatar;
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const posts = await response.json();
+      const nonReplies = posts.filter(post => !post.in_reply_to_id).slice(0,8);
+      const contentElement = document.getElementById('content_4');
+      nonReplies.forEach(post => {
+          const postElement = document.createElement('div');
+          postElement.className = 'red';
+          const pfpElement = document.createElement('img');
+          pfpElement.className = 'pfp_block';
+          pfpElement.src = profilePictureUrl;
+          pfpElement.alt = 'Profile Picture';
+          pfpElement.style.top = "0";
+          const contentDiv = document.createElement('div');
+          contentDiv.className = 'content';
+          contentDiv.innerHTML = post.content;
+          postElement.appendChild(pfpElement);
+          postElement.appendChild(contentDiv);
+          if (post.media_attachments && post.media_attachments.length > 0) {
+              const firstImage = post.media_attachments.find(media => media.type === 'image');
+              if (firstImage) {
+                  const imageElement = document.createElement('img');
+                  const imageLink = document.createElement('a');
+                  imageLink.href = firstImage.url;
+                  imageElement.className = 'attached_image';
+                  imageElement.src = firstImage.url;
+                  imageElement.alt = 'Attached Image';
+                  imageElement.style.borderRadius = '5px';
+                  imageLink.style.margin = 'auto';
+                  imageElement.style.bottom = "0";
+
+                  imageLink.appendChild(imageElement);
+                  postElement.appendChild(imageLink);
+              }
+          }
+          contentElement.appendChild(postElement);
+      });
+  } catch (error) {
+      console.error('Error fetching posts:', error);
+  }
+}
+
+export function popup() {
+  if (!document.getElementById('popup')) {
+      const popupHtml = `
+          <div id="popup" style="
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              position: fixed;
+              z-index: 1000;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 100%;
+              overflow: auto;
+          ">
+              <div style="
+                  background-color: red;
+                  padding: 20px;
+                  border: 1px solid #888;
+                  width: 80%;
+                  max-width: 400px;
+                  position: relative;
+                  border-radius: 12px;
+              ">
+                  <p>
+                      HEY<br>
+                      Welcome to my website, this popup will only appear
+                      when you press 'z' or if it's your first time visiting my website.<br><br>
+                      The controls for this site are as follows:<br>
+                      <t><code>z</code>: spawns the welcome popup<br>
+                      <t><code>x</code>: closes the welcome popup<br>
+                      <t><code>c</code>: follows the link that is on the content currently 'playing' if there is one<br>
+                      <t><code>Up_arrow</code>: unselects the 'cassette'<br>
+                      <t><code>Left_arrow</code>: moves the content left<br>
+                      <t><code>Down_arrow</code>: selects the 'cassette'<br>
+                      <t><code>Right_arrow</code>: moves the content right<br><br>
+                      Remember each cassette has multiple 'tracks' so when you select one you can move left and right within it<br><br>
+                      To close this popup press 'x' or click anywhere on the page that isn't the popup.
+                  </p>
+              </div>
+          </div>
+      `;
+      document.body.insertAdjacentHTML('beforeend', popupHtml);
+      localStorage.setItem('popupShown', 'true');
+      const popup = document.getElementById('popup');
+      window.addEventListener('click', function(event) { 
+          if (event.target === popup) {
+              popup.remove();
+          }
+      });
+  }
+}
--- src/res/js/script.js
+++ src/res/js/script.js
@@ -1,242 +1,31 @@
-import { getdata_spotify } from "./extra/main";
-import { getdata_current } from "./extra/main";
-import { popup } from "./popup";
-
-const cassettes = ["casette_1","casette_2","casette_3","casette_4","casette_5"];
-
-const foundCassettes = cassettes.map(id => document.getElementById(id));
-
-const step = 110;
-const walkman = document.getElementById("walkman");
-const content = document.getElementById("content");
-const titleText = document.getElementById("title-text");
-
-let canMove = true;
-let pos = 0;
-const extraTop = 13;
-let downChild = 0;
-
-function getChildrenArray(element) {
-    return element ? Array.from(element.children) : [];
-}
-
-function setContentOpacity(index, display, opacity) {
-    const currentContent = content.children[checkDown()];
-    if (currentContent) {
-        const childrenArray = getChildrenArray(currentContent);
-        if (childrenArray[index]) {
-            if (display == ""){
-                childrenArray[index].style.opacity = opacity;
-                setTimeout(() => {
-                    childrenArray[index].style.display = display;
-                }, 150);
-            } else {
-                childrenArray[index].style.display = display;
-                setTimeout(() => {
-                    childrenArray[index].style.opacity = opacity;
-                }, 150);
-            }
-        }
-    }
-}
-
-function isAbove(elem1, elem2) {
-    const rect1 = elem1.getBoundingClientRect();
-    const rect2 = elem2.getBoundingClientRect();
-    return rect1.top < rect2.top && rect1.left < rect2.right && rect1.right > rect2.left;
-}
-
-function moveL() {
-    pos++;
-    foundCassettes.forEach(item => {
-        const currentRight = parseInt(item.style.right, 10) || 0;
-        const newRight = currentRight + step;
-
-        item.style.transition = 'right 0.3s';
-        item.style.right = `${newRight + 10}px`;
-        setTimeout(() => {
-            item.style.right = `${newRight}px`;
-            item.style.transition = '';
-        }, 300);
-    });
-}
-
-function moveR() {
-    pos--;
-    foundCassettes.forEach(item => {
-        const currentRight = parseInt(item.style.right, 10) || 0;
-        const newRight = currentRight - step;
-
-        item.style.transition = 'right 0.3s';
-        item.style.right = `${newRight - 10}px`;
-        setTimeout(() => {
-            item.style.right = `${newRight}px`;
-            item.style.transition = '';
-        }, 300);
-    });
-}
-
-function moveD() {
-    foundCassettes.some(item => {
-        if (isAbove(item, walkman)) {
-            canMove = false;
-            const walkmanRect = walkman.getBoundingClientRect();
-            const itemRect = item.getBoundingClientRect();
-            const currentTop = parseInt(window.getComputedStyle(item).top, 10) || 0;
-            const distanceToMove = (walkmanRect.bottom - itemRect.top) / 2;
-            const newTop = currentTop + distanceToMove + extraTop;
-            item.style.transition = 'top 0.3s';
-            item.style.top = `${newTop + 20}px`;
-            setTimeout(() => {
-                item.style.top = `${newTop}px`;
-                item.style.transition = '';
-            }, 300);
-            return true;
-        }
-        return false;
-    });
-}
-
-
-function moveU() {
-    reset();
-    downChild = 0;
-    let item = foundCassettes[checkDown()];
-
-    canMove = true;
-        item.style.transition = 'top 0.3s';
-        item.style.top = `${-20}px`;
-        setTimeout(() => {
-            item.style.top = `${0}px`;
-            item.style.transition = '';
-        }, 300);
-}
-
-function checkDown() {
-    return foundCassettes.findIndex(item => item.style.top !== "0px" && item.style.top);
-}
-
-function checkSelect(index) {
-    const currentCassette = foundCassettes[index];
-    if (currentCassette) {
-        const childrenArray = getChildrenArray(currentCassette);
-        return childrenArray[0]?.textContent;
-    }
-    return "";
-}
-
-function moveR_u() {
-    setContentOpacity(downChild, "", "");
-    const currentContent = content.children[checkDown()];
-    if (currentContent) {
-        const childrenArray = getChildrenArray(currentContent);
-        if (downChild + 1 <= childrenArray.length - 1) {
-            downChild++;
-        } else {
-            downChild = 0;
-        }
-        setContentOpacity(downChild, "flex", "1");
-    }
-}
-
-function moveL_u() {
-    const currentContent = content.children[checkDown()];
-    const childrenArray = getChildrenArray(currentContent);
-    setContentOpacity(downChild, "", "");
-    if (downChild - 1 >= 0) {
-        downChild--;
-    } else {
-        downChild = childrenArray.length - 1;
-    }
-    setContentOpacity(downChild, "flex", "1");
-}
-
-function reset() {
-    const currentContent = content.children[checkDown()];
-    if (currentContent) {
-        const childrenArray = getChildrenArray(currentContent);
-        childrenArray.forEach(item => item.style.opacity = "0");
-    }
-}
-
-function updateTitleText() {
-    titleText.textContent = checkSelect(pos + Math.floor(foundCassettes.length / 2));
-}
+import { 
+    getDataSpotify, 
+    getDataCurrent, 
+    fetchAndDisplayPosts, 
+    popup, 
+    updateTitleText, 
+    handleKeyEvent, 
+    handleGamepadEvent 
+} from "./extra/lib";
 
 let lastExecutionTime = 0;
-var throttleInterval = 300;
+const throttleInterval = 300;
 
-document.addEventListener('keydown', event => handleKeyEvent(event.key));
+document.addEventListener('keydown', event => handleKeyEvent(event.key, lastExecutionTime, throttleInterval));
 
-function handleKeyEvent(key) {
-    const currentTime = Date.now();
-    if (currentTime - lastExecutionTime < throttleInterval) return;
-    lastExecutionTime = currentTime;
+requestAnimationFrame(() => handleGamepadEvent(lastExecutionTime, throttleInterval));
+document.getElementById('touch_left').addEventListener('click', () => handleKeyEvent('ArrowLeft', lastExecutionTime, throttleInterval));
+document.getElementById('touch_right').addEventListener('click', () => handleKeyEvent('ArrowRight', lastExecutionTime, throttleInterval));
+document.getElementById('touch_down').addEventListener('click', () => handleKeyEvent('ArrowDown', lastExecutionTime, throttleInterval));
+document.getElementById('touch_up').addEventListener('click', () => handleKeyEvent('ArrowUp', lastExecutionTime, throttleInterval));
+document.getElementById('touch_c').addEventListener('click', () => handleKeyEvent('c', lastExecutionTime, throttleInterval));
+document.getElementById('touch_x').addEventListener('click', () => handleKeyEvent('x', lastExecutionTime, throttleInterval));
+document.getElementById('touch_z').addEventListener('click', () => handleKeyEvent('z', lastExecutionTime, throttleInterval));
 
-    if (key === 'ArrowLeft' && !document.getElementById('popup')) {
-        if (canMove && pos > -Math.floor(foundCassettes.length / 2)) {
-            moveR();
-            updateTitleText();
-        } else if (!canMove) {
-            moveL_u();
-        }
-    } else if (key === 'ArrowRight' && !document.getElementById('popup')) {
-        if (canMove && pos < Math.floor(foundCassettes.length / 2)) {
-            moveL();
-            updateTitleText();
-        } else if (!canMove) {
-            moveR_u();
-        }
-    } else if (key === 'ArrowDown' && !document.getElementById('popup')) {
-        foundCassettes.forEach(item => {
-            if (isAbove(item, walkman)){
-                let link = item.querySelector('a');
-                if (link) {
-                    window.location.href = link.href;
-                }
-                link = item.querySelector('iframe');
-                if (link) {
-                    window.location.href = link.src;
-                }
-            }
-        });
-        if (canMove){
-            moveD();
-            setContentOpacity(0, "flex", "1");
-        }
-    } else if (key === 'ArrowUp' && !canMove) {
-        moveU();
-    } else if (key === 'c' && !document.getElementById('popup')){
-        if (!canMove) {
-            const currentContent = content.children[checkDown()].children[downChild];
-            if (currentContent) {
-                let link = currentContent.querySelector('a');
-                if (link) {
-                    window.location.href = link.href;
-                }
-                link = currentContent.querySelector('iframe');
-                if (link) {
-                    window.location.href = link.src;
-                }
-            }
-        }
-    } else if (key === 'x'){
-        if (document.getElementById('popup')){
-            document.getElementById('popup').remove();
-        }
-    } else if (key === 'z' && !document.getElementById('popup')){
-        popup();
-    }
+getDataCurrent(document.getElementById("live-1"), document.getElementById("live-2"));
+updateTitleText(0);
+getDataSpotify();
+fetchAndDisplayPosts();
+if (!localStorage.getItem('popupShown')) {
+    popup();
 }
-
-document.getElementById('touch_left').addEventListener('click', () => handleKeyEvent('ArrowLeft'));
-document.getElementById('touch_right').addEventListener('click', () => handleKeyEvent('ArrowRight'));
-document.getElementById('touch_down').addEventListener('click', () => handleKeyEvent('ArrowDown'));
-document.getElementById('touch_up').addEventListener('click', () => handleKeyEvent('ArrowUp'));
-document.getElementById('touch_c').addEventListener('click', () => handleKeyEvent('c'));
-document.getElementById('touch_x').addEventListener('click', () => handleKeyEvent('x'));
-document.getElementById('touch_z').addEventListener('click', () => handleKeyEvent('z'));
-
-updateTitleText();
-getdata_current(document.getElementById("live-1"),document.getElementById("live-2"));
-getdata_spotify();
